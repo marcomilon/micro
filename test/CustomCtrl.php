f6bffed50bcf44d0279b380b1c3e6588c2ea3982
Marco Milon <marco.milon@avantica.net>
2017-11-10 18:41:49 -0500

    New test cases

--- test/CustomCtrl.php
+++ test/CustomCtrl.php
@@ -4,13 +4,13 @@ namespace app\controller;
 
 class CustomCtrl
 {
-    public function action() 
+    public function action($arg1, $arg2) 
     {
-        
+        return "$arg1,$arg2";
     }
     
     public function index() 
     {
-        
+        return "Hello world";
     }
 }
