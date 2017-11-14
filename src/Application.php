3e0e458b6ec1a696ce19b07a6df84f190ce2a85d
Marco Milon <marco.milon@avantica.net>
2017-11-14 17:49:58 -0500

    Clean test cases

--- src/Application.php
+++ src/Application.php
@@ -5,6 +5,8 @@ namespace micro;
 class Application 
 {
     
+    use Response;
+    
     const CONTROLLER_SUFFIX = 'Ctrl';
     const DEFAULT_CONTROLLER = 'DefaultCtrl';
     const DEFAULT_ACTION = 'index';
@@ -29,7 +31,7 @@ class Application
         $this->callAction($controller, $actionName, $queryString);
     }
     
-    public function parseRoute($rParameter) 
+    private function parseRoute($rParameter) 
     {   
         if (preg_match('/^([a-zA-Z]+)\/([a-zA-Z]+)$/', $rParameter, $matches)) {
             $controller = ucfirst(strtolower($matches[1])). 'Ctrl';
@@ -48,20 +50,20 @@ class Application
         ];
     }
     
-    public function instantiateController($controllerName) 
+    private function instantiateController($controllerName) 
     {
         try {
             $controller = '\app\controller\\' . $controllerName;
             $object = new $controller;
             return $object;
         } catch (\Throwable $e) {
-            $this->sendResponse('Not found', 404);
+            $this->send('Not found', 404);
         } catch (\Exception $e) {
-            $this->sendResponse('Not found', 404);
+            $this->send('Not found', 404);
         }
     }
     
-    public function callAction($controller, $action, $arguments = []) 
+    private function callAction($controller, $action, $arguments = []) 
     {
         $hasExpectedParatemers = true;
         if(is_object($controller)) {
@@ -82,23 +84,17 @@ class Application
                 
                 if($hasExpectedParatemers) {
                     $response = call_user_func_array([$controller, $action], $parameters);
-                    $this->sendResponse($response, 200);
+                    $this->send($response, 200);
                 } else {
-                    $this->sendResponse('Bad request', 400);
+                    $this->send('Bad request', 400);
                 }
                 
             } else {
-                $this->sendResponse('Not found', 404);
+                $this->send('Not found', 404);
             }
         }
     }
-    
-    public function sendResponse($body, $status) 
-    {
-        http_response_code($status);
-        echo $body;
-    }
-    
+        
     public function handleError($errno, $errstr, $errfile, $errline)
     {
         if (!(error_reporting() & $errno)) {
@@ -107,13 +103,9 @@ class Application
             return false;
         }
         
-        $this->sendResponse($errfile."[".$errline."]: ". $errstr, 500);
+        $this->send($errfile."[".$errline."]: ". $errstr, 500);
         /* Don't execute PHP internal error handler */
         return true;
-    }
-    
-    private function isResponseSent() {
-        return $this->responseSent === true;
     }
     
 }
